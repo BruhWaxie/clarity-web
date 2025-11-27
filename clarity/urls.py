2811fc8661c43ba55ffa458ee44476e6aa20cffa
BruhWaxie <efirestmax@gmail.com>
2025-11-27 16:45:02 +0200

    Fixed major bags

--- clarity/urls.py
+++ clarity/urls.py
@@ -1,9 +1,9 @@
 """
-URL configuration for {{ project_name }} project.
+URL configuration for clarity project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
-{{ project_name }}s:
+claritys:
 Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
