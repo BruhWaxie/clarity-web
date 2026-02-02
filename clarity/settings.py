f6a2e1e8a9a659960419a298c3964e91f6ea0155
BruhWaxie <efirestmax@gmail.com>
2026-02-02 18:56:51 +0200

    Falsen the DEBUG variable

--- clarity/settings.py
+++ clarity/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-5rh=#tul4uustjy#bekm_
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', True)
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
